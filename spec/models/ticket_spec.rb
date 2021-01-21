e2d334dcd805902b5731ce0698383c108c9192ac
Felipe Arreola <arreolaf@oregonstate.edu>
2021-01-21 11:06:17 -0800

    Test Ticket for 'name', 'description', and 'phone' presence

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+  it "has a name" do
+    expect(Ticket.new).to respond_to(:name)
+  end
+
+  it "has a description" do
+    expect(Ticket.new).to respond_to(:description)
+  end
+
+  it "has a phone" do
+    expect(Ticket.new).to respond_to(:phone)
+  end
+
 end
