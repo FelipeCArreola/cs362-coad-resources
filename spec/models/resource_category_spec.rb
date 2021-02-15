7d372f5497a98ea07dd4f2017d8357d930a945a3
Felipe Arreola <arreolaf@oregonstate.edu>
2021-02-14 17:55:06 -0800

    Test ResourceCategory for name, active attribute, Associations: has many Tickets, has and belongs to many Organizations; Validations: presence of name, and length of name

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,4 +2,31 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
+it 'has a name' do 
+  expect(ResourceCategory.new).to respond_to(:name)
+end
+
+it 'has an \'active\' attribute' do
+  expect(ResourceCategory.new).to respond_to(:active)
+end
+
+describe 'associations' do
+  it 'has many tickets' do
+    expect(ResourceCategory.new).to have_many(:tickets)
+  end
+
+  it 'has and belongs to many organizations' do
+    expect(ResourceCategory.new).to have_and_belong_to_many(:organizations)
+  end
+end
+
+describe 'validations' do
+  it { should validate_presence_of(:name) }
+  it { should validate_length_of(:name).is_at_least(1).is_at_most(255)
+    .on(:create) 
+  }
+  # TODO: validate {uniqueness_of :name, case_sensitive: false}
+end
+
+
 end
