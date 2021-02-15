d909c7386bf64c037964965a1228173f0bf0c655
Felipe Arreola <arreolaf@oregonstate.edu>
2021-02-14 22:49:06 -0800

    Test User for 'attributes' (role, email), Associations: belongs to Organizations; Validations: presence of 'email', length of 'email', expected to allow value

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,31 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it 'has a role' do
-    expect(User.new).to respond_to(:role)
-  end
+  describe "attributes" do
+    it {is_expected.to respond_to(:role) }
+    it {is_expected.to respond_to(:email) }
+  end 
+  # END Attributes
 
-  it 'has an email' do
-    expect(User.new).to respond_to(:email)
+  describe 'association' do
+    it 'belongs to' do
+      expect(User.new).to belong_to(:organization)
+    end
   end
+  # END Associations
 
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_length_of(:email).is_at_least(1).is_at_most(255)
+      .on(:create) 
+    }
+    # not sure if correct utilization of allow_value for verifying format
+    it { is_expected.to allow_value('dog_cat21@gmail.com').for(:email)}
+    
+    # TODO: validate ...
+
+  end
+  # END Validations
+  
 end
+# END TEST
