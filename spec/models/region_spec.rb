96b314b46d1fbdb5b4a8a7f1caeae8e409c879cf
Felipe Arreola <arreolaf@oregonstate.edu>
2021-02-14 18:10:24 -0800

    Test Region for Association: has many Tickets; Validations: presence of name, and length of name

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Region, type: :model do
     expect(Region.new).to respond_to(:name)
   end
 
+  describe 'association' do
+    it 'has many tickets' do
+      expect(Region.new).to have_many(:tickets)
+    end
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(1).is_at_most(255)
+      .on(:create) 
+    }
+    # TODO: validate {uniqueness_of :name, case_sensitive: false}
+  end
 end
