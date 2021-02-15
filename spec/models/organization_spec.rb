b806ba7d8b0f9197d8106a2ffc31878c2888558c
Felipe Arreola <arreolaf@oregonstate.edu>
2021-02-14 20:33:25 -0800

    Test Organization for 'attributes' (name, status, phone, email, primary_name, secondary_name, secondary_phone), Associations: has many Tickets, has many Users, has and belongs to many 'Resource Categories'; Validations: presence of 'attributes', and length of 'name' and 'email'

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,52 +2,50 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
-it "has a name" do
-  expect(Organization.new).to respond_to(:name)
-end
+  attributes_array = [
+    :name,                :status,        :phone, 
+    :email,               :primary_name,  :secondary_name,
+    :secondary_phone     
+  ]
 
-it "has a status" do
-  expect(Organization.new).to respond_to(:status)
-end
+describe "attributes" do
+  attributes_array.each do |attribute|
+    it {is_expected.to respond_to(attribute) }
+  end
+end 
+# END Attributes
 
-it "has a phone" do
-  expect(Organization.new).to respond_to(:phone)
-end
+describe 'associations' do
+  it 'has many tickets' do
+    expect(Organization.new).to have_many(:tickets)
+  end
 
-it "has an email" do
-  expect(Organization.new).to respond_to(:email)
-end
+  it 'has many users' do
+    expect(Organization.new).to have_many(:users)
+  end
 
-it "has a description" do
-  expect(Organization.new).to respond_to(:description)
-end
+  it 'has and belongs to many \'resource categories\'' do
+    expect(Organization.new).to have_and_belong_to_many(:resource_categories)
+  end
+end 
+# END Associations
 
-it "has a rejection reason" do
-  expect(Organization.new).to respond_to(:rejection_reason)
-end
+describe 'validations' do
+  # validate presence
+  attributes_array.each do |attribute|
+    it { is_expected.to validate_presence_of(attribute) } 
+  end 
 
-it "has liability insurance" do
-  expect(Organization.new).to respond_to(:liability_insurance)
-end
+  # validate length 
+  [:name, :email ].each do |attribute|
+    it { is_expected.to validate_length_of(attribute).is_at_least(1).is_at_most(255)
+      .on(:create) 
+    } 
+  end 
 
-it "has a primary name" do
-  expect(Organization.new).to respond_to(:primary_name)
+  # TODO: validate {uniqueness_of :name, case_sensitive: false}
 end
-
-it "has a secondary name" do
-  expect(Organization.new).to respond_to(:secondary_name)
-end
-
-it "has a secondary phone" do
-  expect(Organization.new).to respond_to(:secondary_phone)
-end
-
-it "has a title" do
-  expect(Organization.new).to respond_to(:title)
-end
-
-it "has transportation" do
-  expect(Organization.new).to respond_to(:transportation)
-end
+# END Validations
 
 end
+# END TEST
