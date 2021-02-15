1d059cf1397b15df5f6734b85fc48f34b69b3916
Felipe Arreola <arreolaf@oregonstate.edu>
2021-02-14 20:37:25 -0800

    updated test format to describe blocks

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,21 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
-  it "has a name" do
-    expect(Region.new).to respond_to(:name)
-  end
+  describe "attributes" do
+    it {is_expected.to respond_to(:name) }
+  end 
+  # END Attributes
 
   describe 'association' do
     it 'has many tickets' do
       expect(Region.new).to have_many(:tickets)
     end
   end
+  # END Associations
 
   describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_length_of(:name).is_at_least(1).is_at_most(255)
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(255)
       .on(:create) 
     }
     # TODO: validate {uniqueness_of :name, case_sensitive: false}
+
   end
+  # END Validations
+  
 end
+# END TEST
