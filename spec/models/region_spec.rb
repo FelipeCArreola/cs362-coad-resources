937c6373bab76400de5fb15e4dcd78154b850e35
Felipe Arreola <arreolaf@oregonstate.edu>
2021-03-14 18:03:02 -0700

    region_spec added test for validation of 'name' uniqueness, test for member functions, test for static class functions

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+  let(:region_test) {Region.new(name: 'region_name')}
+  let(:region) {build(:region)}
+
   describe "attributes" do
     it {is_expected.to respond_to(:name) }
   end 
@@ -19,10 +22,24 @@ RSpec.describe Region, type: :model do
     it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(255)
       .on(:create) 
     }
-    # TODO: validate {uniqueness_of :name, case_sensitive: false}
-
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive() }
   end
   # END Validations
+
+  describe 'member function(s):' do
+    it 'has return name of region' do
+       expect(region_test.to_s).to eq('region_name') 
+    end  
+  end
+  # END Member Functions
+
+  describe 'static (class) function(s):' do
+    it 'has declares unspecified region' do
+      expect(Region.unspecified.name).to eq('Unspecified') 
+    end
+  end
+  # #END Static (Class) Functions
+  
   
 end
 # END TEST
